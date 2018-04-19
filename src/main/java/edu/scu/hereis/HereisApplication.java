369fa71722fa5b7195f61cbe44cdd85ebd7072d0
zhouMac <LazyZhou1997@gmail.com>
2018-04-19 23:25:35 +0800

    初始化仓库

---
+++ src/main/java/edu/scu/hereis/HereisApplication.java
@@ -0,0 +1,12 @@
+package edu.scu.hereis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HereisApplication.class, args);
+	}
+}
