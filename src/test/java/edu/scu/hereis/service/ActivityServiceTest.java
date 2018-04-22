41d4b7e0290f70669c58c1788c538f6a7e60191a
LLJQ <1634160753@qq.com>
2018-04-22 21:55:11 +0800

    Add files via upload

---
+++ src/test/java/edu/scu/hereis/service/ActivityServiceTest.java
@@ -0,0 +1,44 @@
+package edu.scu.hereis.service;
+
+import edu.scu.hereis.entity.Activity;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Date;
+
+/**
+ * 测试通过
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class ActivityServiceTest {
+
+    @Autowired
+    private ActivityService activityService;
+
+    @Test
+    public void insertActivity() throws Exception {
+        Activity activity = new Activity();
+        activity.setId(1);
+        activity.setSpotId(1);
+        activity.setIntro("something");
+        activity.setBeginTime(new Date());
+        activity.setEndTime(new Date());
+        activity.setName("some");
+
+        activityService.insertActivity(activity);
+    }
+
+    @Test
+    public void deleteActivity() throws Exception {
+
+        activityService.deleteActivity(1);
+    }
+
+}
