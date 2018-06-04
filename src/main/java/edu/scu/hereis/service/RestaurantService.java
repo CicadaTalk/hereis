9a6a02769a127f53189c6414f385eb97376b96d3
LLJQ <1634160753@qq.com>
2018-06-04 21:53:59 +0800

    Add files via upload

--- src/main/java/edu/scu/hereis/service/RestaurantService.java
+++ src/main/java/edu/scu/hereis/service/RestaurantService.java
@@ -2,11 +2,14 @@ package edu.scu.hereis.service;
 
 import edu.scu.hereis.dao.RestaurantMapper;
 import edu.scu.hereis.entity.Restaurant;
+import edu.scu.hereis.entity.RestaurantExample;
 import edu.scu.hereis.exception.RestaurantServiceException;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * 餐馆相关操作的服务类
@@ -56,6 +59,31 @@ public class RestaurantService {
         restaurantMapper.deleteByPrimaryKey(spotId);
 
         return;
+    }
+
+    /**
+     * 根据传入的spotId判断该餐馆是否已经存在
+     * @param spotId 餐馆id
+     * @return 存在返回false, 不存在返回true
+     */
+    @Transactional
+    public boolean restaurantExists(int spotId) {
+
+        //检查输入
+        if (spotId <= 0) {
+            throw new RestaurantServiceException(RestaurantServiceException.INVALID_INPUT);
+        }
+
+        // 查找
+        RestaurantMapper restaurantMapper = sqlSession.getMapper(RestaurantMapper.class);
+        RestaurantExample restaurantExample = new RestaurantExample();
+        restaurantExample.createCriteria().andSpotIdEqualTo(spotId);
+        List<Restaurant> list = restaurantMapper.selectByExample(restaurantExample);
+        if (list.size() > 0) {
+            return false;
+        }
+
+        return true;
     }
 
 
